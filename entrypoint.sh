fe8b7082238861cb6cfe7783a11394278125f41f
Hiroaki Yoshimura <yhiroaky@amazon.co.jp>
2023-11-14 15:14:09 +0900

    ✨ Update: Docker on colima で実行ができるように各種フできるように各種ファイルの設定ように各種ファイルの設定を修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
+#!/bin/sh
 # create credentials
+set -e
 mkdir -p /root/.aws
-echo -e "[default]\naws_access_key_id = $AWS_ACCESS_KEY_ID\naws_secret_access_key = $AWS_SECRET_ACCESS_KEY" > /root/.aws/credentials
-echo -e "[default]\nregion = $AWS_DEFAULT_REGION" > /root/.aws/config
+echo "[default]\naws_access_key_id = $AWS_ACCESS_KEY_ID\naws_secret_access_key = $AWS_SECRET_ACCESS_KEY" > /root/.aws/credentials
+echo "[default]\nregion = $AWS_DEFAULT_REGION" > /root/.aws/config
 
 uvicorn --factory src.backend.langflow.main:create_app --host 0.0.0.0 --port 7860 --reload
